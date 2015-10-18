3c398b62c5fa882f3bb5bd0c8ce99eafe459afe0
FelipeCrispim <felipe.ccrispim@gmail.com>
2015-10-18 16:45:51 -0300

    Atualizado FIFO

--- Escalonamento_FCFS/src/FIFO.java
+++ Escalonamento_FCFS/src/FIFO.java
@@ -1,4 +1,4 @@
-import java.io.*;  
+import java.io.*;
 import java.util.*;
 
 public class FIFO {
@@ -11,62 +11,98 @@ public class FIFO {
 	private static List<Task> tarefasExecutando;
 	private static List<Task> tarefasTerminadas;
 
-	public static void scheduler(int time){
-		System.out.printf("\n_________segundo: %d_____________________ \n\n", time);
-		
+	public static void scheduler(int time) {
+		// System.out.printf("\n_________segundo: %d_____________________ \n\n",
+		// time);
+
 		List<Task> taskToRemove = new ArrayList<>();
 
-		if(!tarefasNovas.isEmpty()){
-			for(int i=0; i<tarefasNovas.size(); i++){
-				System.out.println("Processo a ser analisado: "+tarefasNovas.get(i).id);
-				if(tarefasNovas.get(i).date == time){
-					System.out.println(tarefasNovas.get(i).id + " ficou pronto");
-					tarefasNovas.get(i).status = "pronta";
+		if (!tarefasNovas.isEmpty()) {
+			for (int i = 0; i < tarefasNovas.size(); i++) {
+				// System.out.println("Processo a ser analisado:
+				// "+tarefasNovas.get(i).getID());
+				if (tarefasNovas.get(i).getDate() == time) {
+					// System.out.println(tarefasNovas.get(i).getID() + " ficou
+					// pronto");
+					tarefasNovas.get(i).setStatus("ready");
 					tarefasProntas.add(tarefasNovas.get(i));
 					taskToRemove.add(tarefasNovas.get(i));
 				}
 			}
 		}
-		if(!taskToRemove.isEmpty()){
-			for(int i=0; i<taskToRemove.size(); i++){
+		if (!taskToRemove.isEmpty()) {
+			for (int i = 0; i < taskToRemove.size(); i++) {
 				tarefasNovas.remove(taskToRemove.get(i));
 			}
 		}
-		
-		if(tarefasExecutando.isEmpty() != true){
-			for(int i=0; i<tarefasExecutando.size(); i++){
-				if(tarefasExecutando.get(i).duration == tarefasExecutando.get(i).executingTime){
-					System.out.println(tarefasExecutando.get(i).id + " terminou");
 
-					tarefasExecutando.get(i).status = "terminada";
+		if (tarefasExecutando.isEmpty() != true) {
+			for (int i = 0; i < tarefasExecutando.size(); i++) {
+				if (tarefasExecutando.get(i).getTotalTime() == tarefasExecutando.get(i).getElapsedTime()) {
+					// System.out.println(tarefasExecutando.get(i).getID() + "
+					// terminou");
+
+					tarefasExecutando.get(i).setStatus("finished");
 					tarefasTerminadas.add(tarefasExecutando.get(i));
 					tarefasExecutando.remove(i);
+
 				} else {
-					tarefasExecutando.get(i).executingTime += 1;
-					System.out.println(tarefasExecutando.get(i).id + " ainda executando");
+					tarefasExecutando.get(i).addElapsedTime(1);
+					// System.out.println(tarefasExecutando.get(i).getID() + "
+					// ainda executando");
 				}
 			}
-		} else if(!tarefasProntas.isEmpty()){
+		}
+		if (!tarefasProntas.isEmpty() && tarefasExecutando.isEmpty()) {
 			int highPriority = 0;
 			int indexTask = 0;
-			for(int i=0; i<tarefasProntas.size(); i++){
-				//Procura saber quem tem maior prioridade pra usar o processador.
-				//A de maior prioridade sai da lista de prontas para a de executando.
-				if(tarefasProntas.get(i).priority > highPriority){
-					highPriority = tarefasProntas.get(i).priority;
+			for (int i = 0; i < tarefasProntas.size(); i++) {
+				// Procura saber quem tem maior prioridade pra usar o
+				// processador.
+				// A de maior prioridade sai da lista de prontas para a de
+				// executando.
+				if (tarefasProntas.get(i).getPriority() > highPriority) {
+					highPriority = tarefasProntas.get(i).getPriority();
 					indexTask = i;
 				}
 			}
-			System.out.println(tarefasProntas.get(indexTask).id + " começou a executar");
-			tarefasProntas.get(indexTask).status = "executando";
-			tarefasProntas.get(indexTask).executingTime += 1;
+			tarefasProntas.get(indexTask).setStatus("executando");
+			tarefasProntas.get(indexTask).addElapsedTime(1);
 			tarefasExecutando.add(tarefasProntas.get(indexTask));
 			tarefasProntas.remove(indexTask);
 		}
-		
-//		if(tarefasExecutando.isEmpty()){
-//			System.out.println(Integer.toString(time)+"- "+Integer.toString(time+1)+"   "+"--"+"  --"+"  --"+"  --");
+
+		System.out.print(Integer.toString(time) + "- " + Integer.toString(time + 1) + "    ");
+
+//		for (int i = 0; i < numberOfProcesses; i++) {
+//			if(!tarefasExecutando.isEmpty() && Integer.parseInt(tarefasExecutando.get(0).getID()) == i){
+//				System.out.print("##  ");
+//			} else if (!tarefasProntas.isEmpty()){
+//				for (int j = 0; j < tarefasProntas.size(); j++) {
+//					if (Integer.parseInt(tarefasProntas.get(j).getID()) == i) {
+//						System.out.print("--  ");
+//						j = tarefasProntas.size();
+//					}
+//				}
+//			} else {
+//				System.out.print("    ");
+//			}
 //		}
+
+		if (tarefasExecutando.isEmpty() && !tarefasProntas.isEmpty()) {
+			for (int i = 0; i < tarefasProntas.size(); i++) {
+				System.out.print("--  ");
+			}
+		} else {
+			for (int i = 0; i < numberOfProcesses; i++) {
+				if (Integer.parseInt(tarefasExecutando.get(0).getID()) == i) {
+					System.out.print("##  ");
+				} else {
+					System.out.print("--  ");
+				}
+			}
+		}
+		System.out.print("\n");
 
 	}
 
@@ -77,7 +113,7 @@ public class FIFO {
 		tarefasProntas = new ArrayList<>();
 		tarefasTerminadas = new ArrayList<>();
 
-		try{  
+		try {
 			Scanner in = new Scanner(m_file);
 
 			while (in.hasNextLine()) {
@@ -86,35 +122,33 @@ public class FIFO {
 			}
 			in = new Scanner(m_file);
 
-			try{
-				for(int linha=0 ; linha < numberOfProcesses ; linha++){
+			try {
+				for (int linha = 0; linha < numberOfProcesses; linha++) {
 					Task tarefa = new Task();
-					tarefa.date = in.nextInt();
-					tarefa.duration = in.nextInt();
-					tarefa.priority = in.nextInt();
-					tarefa.status = "teste";
-					tarefa.id = linha;
+					tarefa.setDate(in.nextInt());
+					tarefa.setTotalTime(in.nextInt());
+					tarefa.setPriority(in.nextInt());
+					tarefa.setID(Integer.toString(linha));
 
 					tarefasNovas.add(tarefa);
 				}
-				System.out.printf("tamanho da lista: %d \n\n", tarefasNovas.size());
-				in.close();  
-			}  
-			catch(NoSuchElementException nex){
-				System.err.println("Erro na criação da lista com os processos.");  
-			}    
+				// System.out.printf("tamanho da lista: %d \n\n",
+				// tarefasNovas.size());
+				in.close();
+			} catch (NoSuchElementException nex) {
+				System.err.println("Erro na criação da lista com os processos.");
+			}
 
-		}  
-		catch(IOException ex){
-			ex.printStackTrace();  
+		} catch (IOException ex) {
+			ex.printStackTrace();
 			System.out.println("Erro ao ler o arquivo.");
 		}
 
 		System.out.print("tempo   ");
-		for (int i=0; i<numberOfProcesses; i++){
+		for (int i = 0; i < numberOfProcesses; i++) {
 			System.out.printf("P%d  ", i);
 		}
-		System.out.println("\n");
+		System.out.print("\n");
 
 		Timer timer = new Timer();
 		timer.scheduleAtFixedRate(new TimerTask() {
@@ -122,17 +156,17 @@ public class FIFO {
 				scheduler(elapsedTime);
 				elapsedTime++;
 			}
-		}, 0, 3000);
+		}, 0, 1000);
 
 	}
 
 }
 
-class Task {
-	public int id; 
-	public int date;//em que tempo sera criada
-	public int duration; //quanto tempo precisa no processador
-	public int executingTime = 0; //tempo de processamento ja decorrido
-	public int priority;
-	public String status;
-};
+// class Task {
+// public int id;
+// public int date;//em que tempo sera criada
+// public int duration; //quanto tempo precisa no processador
+// public int executingTime = 0; //tempo de processamento ja decorrido
+// public int priority;
+// public String status;
+// };
